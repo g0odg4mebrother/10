997ff28b23606408be73de3e3aaabd6c7d58a60f
g0odg4mebrother <kirilcukvlad30@gmail.com>
2024-09-23 23:09:19 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿// project2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+    setlocale(LC_ALL, "Russian");
+    int height, times;
+    cout << " введите высоту\n";
+    cin >> height;
+    times = sqrt(2 * height / 9.8);
+    cout << "ответ" << times << " c";
+    return 0;
+
+
+
+}
